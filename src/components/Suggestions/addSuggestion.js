6cd7683c7e5064fd39decd98ae5d5cb347c0c42c
Jonas.J <jan.jonas.johansson@gmail.com>
2018-07-16 00:49:55 +0200

    image uploads started

--- src/components/Suggestions/addSuggestion.js
+++ src/components/Suggestions/addSuggestion.js
@@ -51,6 +51,59 @@ export default class AddSuggestion extends Component {
       })
   }
 
+
+  resizeImage(file){
+    // Create an image
+    let img = document.createElement("img");
+
+    // Create a file reader
+    var reader = new FileReader();
+
+    // Set the image once loaded into file reader
+    reader.onload = function(e) {
+      img.src = e.target.result;
+      console.log(img)
+      var canvas = document.createElement("canvas");
+      //var canvas = $("<canvas>", {"id":"testing"})[0];
+      var ctx = canvas.getContext("2d");
+      ctx.drawImage(img, 0, 0);
+      console.log(canvas.toDataURL("image/png"))
+      var MAX_WIDTH = 400;
+      var MAX_HEIGHT = 400;
+      var width = img.width;
+      var height = img.height;
+
+      if (width > height) {
+        if (width > MAX_WIDTH) {
+          height *= MAX_WIDTH / width;
+          width = MAX_WIDTH;
+        }
+      } else {
+        if (height > MAX_HEIGHT) {
+          width *= MAX_HEIGHT / height;
+          height = MAX_HEIGHT;
+        }
+      }
+      console.log(canvas.toDataURL("image/png"))
+      console.log(width)
+      canvas.width = width;
+      canvas.height = height;
+
+      ctx = canvas.getContext("2d");
+      ctx.drawImage(img, 0, 0, width, height);
+
+      let dataurl = canvas.toDataURL("image/png");
+      document.getElementById('output').src = dataurl;
+      console.log(dataurl)
+    }
+    // Load files into file reader
+    reader.readAsDataURL(file);
+  }
+
+  uploadImage(file){
+    console.log(file)
+  }
+
   render () {
     return (
       <form onSubmit={this.handleSubmit}>
@@ -87,6 +140,19 @@ export default class AddSuggestion extends Component {
           <textarea value={this.state.description}
                     onChange={e => this.setState({description: e.target.value})}/>
         </label>
+
+        <label>
+          ladda upp foto:
+          <input type={"file"}
+                 onChange={(e) => {
+                   console.log(e.target.files)
+                   this.resizeImage(e.target.files[0])}}/>
+        </label>
+
+        <img src="" id="output"/>
+
+
+
 
         <input type="submit" value="Submit"/>
       </form>
